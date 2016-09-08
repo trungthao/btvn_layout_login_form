4f670faa20f90d2fdf0d8107046fab52065ffa5c
trungthao <thao.duongtrung@gmail.com>
2016-09-08 18:09:37 +0700

    login_form_ver1

---
+++ app/src/main/java/com/uet/trungthao/login_buoi3/MainActivity.java
@@ -0,0 +1,15 @@
+package com.uet.trungthao.login_buoi3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by JiH on 08/09/2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
